79824503c42f51dd2fe351a44cb6d97b6f9147f7
Yaneya-Yaneya <yukoyaneya@gmail.com>
2024-09-07 11:52:15 +0900

    ページトップに戻るボタンを追加

--- lp02/assets/js/lp02.js
+++ lp02/assets/js/lp02.js
@@ -1,8 +1,38 @@
 // JavaScript Document
 
-$(function(){
-$('.accordion_q').click(function(){
-  $(this).next().slideToggle(300); 
-  $(this).toggleClass('arrow');
+$(function () {
+  $('.accordion_q').click(function () {
+    $(this).next().slideToggle(300);
+    $(this).toggleClass('arrow');
+  });
 });
+
+function PageTopAnime() {
+  var scroll = $(window).scrollTop();
+  if (scroll >= 200) {
+    $('#page-top').removeClass('DownMove');
+    $('#page-top').addClass('UpMove');
+  } else {
+    if ($('#page-top').hasClass('UpMove')) {
+      $('#page-top').removeClass('UpMove');
+      $('#page-top').addClass('DownMove');
+    }
+  }
+}
+
+
+$(window).scroll(function () {
+  PageTopAnime();
+});
+
+
+$(window).on('load', function () {
+  PageTopAnime();
+});
+
+$('#page-top a').click(function () {
+  $('body,html').animate({
+    scrollTop: 0
+  }, 1000);
+  return false;
 });
